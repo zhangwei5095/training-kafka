f86ac6719ee45517c3eace42dba5120208183a3d
Percy0601 <2008dreams@2008.sina.com>
2016-05-18 22:25:52 +0800

    增加启动程序

---
+++ training-kafka-producer/src/main/java/io/training/TrainingKafkaApplication.java
@@ -0,0 +1,17 @@
+package io.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动程序
+ *
+ * Created by percy on 16-5-18.
+ */
+@SpringBootApplication
+public class TrainingKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingKafkaApplication.class, args);
+    }
+}
